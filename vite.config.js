41185c346d2a04dd1c9a7456bd0610cb9e04dadc
Shaunak <ssnagvenkar@gmail.com>
2024-03-13 00:42:50 +0530

    Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { sass } from '@sveltejs/adapter-static';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    sass(),],
+    react(),],
 })
