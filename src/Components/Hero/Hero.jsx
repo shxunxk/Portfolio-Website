e9b9abd3689691ffcc44042ca5a14a1df6e65c64
Shaunak <ssnagvenkar@gmail.com>
2024-03-14 00:37:07 +0530

    changes to contacts

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -31,6 +31,32 @@ const sliderVariants = {
 },
 }
 
+// const buttonVar = {
+//   initial: {
+//     y: 0,
+//   },
+//   up: {
+//     y: 100,
+//     transition: {
+//       duration: 1,
+//       ease: "easeInOut",
+//     },
+//   },
+//   down: {
+//     y: 0,
+//     transition: {
+//       duration: 1,
+//       ease: "easeInOut",
+//     },
+//   },
+// };
+// setInterval(() => {
+//   buttonVar.animate = buttonVar.up;
+//   setTimeout(() => {
+//     buttonVar.animate = buttonVar.down;
+//   }, 1000);
+// }, 2000);
+
 export default function hero() {
   return (
     <div className='hero'>
@@ -39,10 +65,10 @@ export default function hero() {
           <motion.h1 variants={textVariants}><p>Machine Learning .<br/> Data Science and Analytics .<br/> Web Development .</p></motion.h1>
           <motion.div variants={textVariants} className='buttons'>
             <Link to='/latest-work'>
-              <button className='button'> Latest Work </button>
+              <motion.button className='button' variants={buttonVar} initial="initial" animate="animate"> Latest Work </motion.button>
             </Link>
             <Link to='/about'>
-              <button className='button'> About Me </button>
+              <motion.button className='button' variants={buttonVar} initial="initial" animate="animate"> About Me </motion.button>
             </Link>
           </motion.div>
         </motion.div>
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -51,11 +51,22 @@ export default function About() {
                 <motion.ul style={{fontSize: '2rem'}} initial={{ x: 0 }} animate={{ x: scrollY > 2600 ? 2000 : 0 }} transition={{ duration: 0.7 }}>Based in Goa, India</motion.ul>
                 <motion.ul style={{fontSize: '2rem'}} initial={{ x: 0 }} animate={{ x: scrollY > 2600 ? 2000 : 0 }} transition={{ duration: 0.7 }}>Pursuing Bachelors Degree in the field of Computer Science Engineering from Vellore Institute of Technology, Vellore</motion.ul>
               </section>
-              <section id='sec' className='Contact'>
-                <motion.h2 style={{fontSize: '5rem', display: 'flex', flexDirection: 'column'}} initial={{ x: 0 }} animate={{ x: scrollY > 3200 ? -2000 : 0 }} transition={{ duration: 0.7 }}>Contact</motion.h2>
-                <motion.ul style={{fontSize: '2rem'}} initial={{ x: 0 }} animate={{ x: scrollY > 3200 ? -2000 : 0 }} transition={{ duration: 0.7 }}>Email: <Link to='mailto:ssnagvenkar@gmail.com'>ssnagvenkar@gmail.com</Link></motion.ul>
-              </section>
         </div>
+        <section className='contact'>
+                {/* <motion.h2 style={{fontSize: '5rem', display: 'flex', flexDirection: 'column'}} initial={{ x: 0 }} animate={{ x: scrollY > 3200 ? -2000 : 0 }} transition={{ duration: 0.7 }}>Contact</motion.h2>
+                <motion.ul style={{fontSize: '2rem'}} initial={{ x: 0 }} animate={{ x: scrollY > 3200 ? -2000 : 0 }} transition={{ duration: 0.7 }}>Email: <Link to='mailto:ssnagvenkar@gmail.com'>ssnagvenkar@gmail.com</Link></motion.ul> */}
+                <div className='sub' id='sub1'>
+                  <h3>Get in touch</h3>
+                  <div className='inner'>
+                    <p >Email: <Link className='email' to='mailto:ssnagvenkar@gmail.com'>ssnagvenkar@gmail.com</Link></p>
+                  </div>
+                </div>
+                <div className='sub' id='sub2'>
+                  <p><Link to='/'>Home</Link></p>
+                  <p><Link to='/latest-work'>Latest Work</Link></p>
+                </div>
+                <div className='sub' id='sub3'></div>
+              </section>
       </div>
     </div>
   )
